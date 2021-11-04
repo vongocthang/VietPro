e81a7386350d559b93416678682d8b3bf9caf14e
Võ Ngọc Thắng <vongocthang160199@gmail.com>
2021-11-04 22:11:00 +0700

    a

---
+++ Unity2D_VietPro_281021/Assets/Script/QuanLyPhong.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class QuanLyPhong : MonoBehaviour
+{
+    public Transform contentTransform;
+    public GameObject tempplateContent;
+    public int countRoom = 1;
+    public int quantilyRoom;
+
+    public string inputRoomName;
+    public string inputPassWord;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CreateRoom(quantilyRoom);
+    }
+
+    public void CreateRoom(int quantily)
+    {
+        while (countRoom <= quantily)
+        {
+            GameObject phong = Instantiate(tempplateContent, contentTransform);
+            phong.SetActive(true);
+            if (countRoom < 10)
+            {
+                phong.transform.GetChild(0).GetComponent<TMP_Text>().text = "ID: 0" + (countRoom).ToString();
+            }
+            else
+            {
+                phong.transform.GetChild(0).GetComponent<TMP_Text>().text = "ID: " + (countRoom).ToString();
+            }
+            phong.transform.GetChild(1).GetComponent<Button>().onClick.AddListener(() => JoinRoom(int.Parse(phong.transform.GetChild(0).GetComponent<TMP_Text>().text.Substring(4, 2))));
+
+            countRoom++;
+        }
+    }
+
+    public void JoinRoom(int idRoom)
+    {
+        Debug.Log("Vao phong: " + idRoom);
+    }
+
+    public void ReadStringInputRoomName(string s)
+    {
+        inputRoomName = s;
+    }
+
+    public void ReadStringInputPassWord(string s)
+    {
+        inputPassWord = s;
+    }
+
+    public void ShowStringInput()
+    {
+        Debug.Log("Ten phong: " + inputRoomName);
+        Debug.Log("Mat khau: " + inputPassWord);
+
+        GameObject phong = Instantiate(tempplateContent, contentTransform);
+        phong.SetActive(true);
+        if (countRoom < 10)
+        {
+            phong.transform.GetChild(0).GetComponent<TMP_Text>().text = "ID: 0" + (countRoom).ToString() + " Ten Phong: " + inputRoomName;
+        }
+        else
+        {
+            phong.transform.GetChild(0).GetComponent<TMP_Text>().text = "ID: " + (countRoom).ToString() + "Ten Phong: " + inputRoomName;
+        }
+        phong.transform.GetChild(1).GetComponent<Button>().onClick.AddListener(() => JoinRoom(int.Parse(phong.transform.GetChild(0).GetComponent<TMP_Text>().text.Substring(4, 2))));
+
+        countRoom++;
+    }
+}
---
+++ Unity2D_VietPro_281021/Assets/Script/ScrollView.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ScrollView : MonoBehaviour
+{
+    public class item
+    {
+        public string anh;
+        public string ten;
+    }
+    public GameObject btnDan;
+    public ScrollRect scrollMenu;
+    public Object[] mangAnh;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        List<item> danhSach = new List<item>();
+        item _item;
+
+        _item = new item();
+        _item.anh = "1";
+        _item.ten = "Dan loai 1";
+        danhSach.Add(_item);
+
+        _item = new item();
+        _item.anh = "2";
+        _item.ten = "Dan loai 2";
+
+        danhSach.Add(_item);
+
+        mangAnh = Resources.LoadAll("Textures", typeof(Texture2D));
+
+        GameObject newbtnDan;
+        Texture2D anhTimThay = null;
+        foreach (item pt in danhSach)
+        {
+            newbtnDan = Instantiate(btnDan, scrollMenu.transform.GetChild(0).GetChild(0).transform, true) as GameObject;
+            newbtnDan.SetActive(true);
+            newbtnDan.transform.GetChild(0).GetComponent<TMP_Text>().text = pt.ten;
+            Debug.Log("OK");
+            for(int i=0; i < mangAnh.Length; i++)
+            {
+                Debug.Log("Ten anh: " + mangAnh[i].name);
+                Debug.Log("pt: " + pt.anh);
+                if (mangAnh[i].name == pt.anh)
+                {
+                    anhTimThay = mangAnh[i] as Texture2D;
+                    Rect rec = new Rect(0, 0, anhTimThay.width, anhTimThay.height);
+                    Debug.Log("button: " + newbtnDan.transform.GetChild(1).name);
+                    newbtnDan.transform.GetChild(1).GetComponent<Image>().sprite = Sprite.Create(anhTimThay, rec, new Vector2(0.5f, 0.5f), 100);
+                    break;
+                }
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
