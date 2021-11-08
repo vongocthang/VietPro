2afec7b48c80d8ab7ad850ad70d67257431f04c4
Võ Ngọc Thắng <vongocthang160199@gmail.com>
2021-11-08 17:23:28 +0700

    a

--- Unity2D_VietPro_281021/Assets/Script/DragAndDropUI.cs
+++ Unity2D_VietPro_281021/Assets/Script/DragAndDropUI.cs
@@ -8,11 +8,14 @@ public class DragAndDropUI : MonoBehaviour
     public bool onUI = false;
     public bool drag = false;
     Camera cam;
+    public RectTransform rect;
+    public RectTransform target;
 
     // Start is called before the first frame update
     void Start()
     {
         cam = Camera.main;
+        rect = this.GetComponent<RectTransform>();
     }
 
     // Update is called once per frame
@@ -20,9 +23,23 @@ public class DragAndDropUI : MonoBehaviour
     {
         if (drag == true)
         {
-            Debug.Log("start drag");
-          //  this.transform.position = cam.i(Input.mousePosition);
-           this.transform.position = (Input.mousePosition);
+            Debug.Log("start drag: ");
+            Debug.Log("mouse: " + Input.mousePosition);
+            //this.transform.position = cam.i(Input.mousePosition);
+            rect.anchoredPosition = (Input.mousePosition);
+            Debug.Log("UI: " + Input.mousePosition);
+        }
+        if (drag == false)
+        {
+            Vector3 a = rect.anchoredPosition;
+            Vector3 b = target.anchoredPosition;
+            if (Vector3.Distance(a, b) <= 100)
+            {
+                rect.anchoredPosition = Vector2.Lerp(rect.anchoredPosition, target.anchoredPosition, 
+                    5 * Time.deltaTime);
+                Debug.Log("Ghep vao");
+            }
+            //rect.anchoredPosition = Vector2.Lerp(rect.anchoredPosition, target.anchoredPosition, 1);
         }
     }
 
@@ -31,5 +48,8 @@ public class DragAndDropUI : MonoBehaviour
         drag = true;
     }
 
-
+    public void EndDraw()
+    {
+        drag = false;
+    }
 }
