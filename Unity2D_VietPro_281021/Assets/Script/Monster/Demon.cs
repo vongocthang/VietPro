ff2c74585f0942710976dc174fe45eb301c39380
Võ Ngọc Thắng <vongocthang160199@gmail.com>
2021-11-19 17:33:31 +0700

    a

---
+++ Unity2D_VietPro_281021/Assets/Script/Monster/Demon.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Demon : MonoBehaviour
+{
+    public bool follow;
+    public GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void FollowPlayer()
+    {
+        if (follow == true)
+        {
+            float x = player.transform.position.x;
+            Vector2 temp = new Vector2(x, transform.position.y);
+            transform.position = Vector2.Lerp(transform.position, temp, 
+                10 * Time.deltaTime);
+        }
+    }
+}
---
+++ Unity2D_VietPro_281021/Assets/Script/Monster/VungPhatHien.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VungPhatHien : MonoBehaviour
+{
+    Demon demon;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        demon = transform.GetComponentInParent<Demon>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Debug.Log("Phat hien Player");
+            demon.follow = true;
+            demon.player = collision.gameObject;
+        }
+    }
+}
