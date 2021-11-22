3d7df1262720e44b84b59dc084cf9c1ef1392a4e
Võ Ngọc Thắng <vongocthang160199@gmail.com>
2021-11-22 23:55:27 +0700

    a

---
+++ Unity2D_VietPro_281021/Assets/Script/New Folder/BoxCast_CircleCast.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxCast_CircleCast : MonoBehaviour
+{
+    [SerializeField]
+    LayerMask CheckVaCham;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit2D hit = Physics2D.BoxCast(transform.position, new Vector2(10f, 10f), 0f,
+            Vector2.right, 5f, CheckVaCham);
+        //RaycastHit2D hit = Physics2D.CircleCast(transform.position, 5f, Vector2.right, 5f, CheckVaCham);
+
+        if (hit)
+        {
+            Debug.Log("Va cham voi: " + hit.transform.gameObject.name);
+        }
+    }
+}
