ea4f62320180a87646b5481c59a5cc866a2c3872
Võ Ngọc Thắng <vongocthang160199@gmail.com>
2021-11-24 22:30:13 +0700

    a

--- Unity2D_VietPro_281021/Assets/Script/PoolBullet.cs
+++ Unity2D_VietPro_281021/Assets/Script/PoolBullet.cs
@@ -4,7 +4,24 @@ using UnityEngine;
 
 public class PoolBullet : MonoBehaviour
 {
-    public GameObject[] bullet;
+    [System.Serializable]
+    public class Pool
+    {
+        public string name;
+        public GameObject prefab;
+        public int size;
+    }
+
+    public static PoolBullet instance;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    public List<Pool> pool;
+
+    Dictionary<string, Queue<GameObject>> poolDictionary;
 
     // Start is called before the first frame update
     void Start()
